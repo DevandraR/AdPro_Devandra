3d383d3a18825b3e72a2e45baa1d85ebc7959b63
Devandra <devandra271203@gmail.com>
2024-03-05 05:00:25 +0700

    [GREEN] Implement Order model

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.adarshr.test-logger") version "4.0.0" // added test logger
 }
 
 group = "id.ac.ui.cs.advprog"
